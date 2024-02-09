ba636a59d02d313d185642a9362a74608dc3add1
VertoX <128989057+VertoX6@users.noreply.github.com>
2024-02-09 13:19:40 +0100

    Update main.js

--- weather-app/main.js
+++ weather-app/main.js
@@ -2,7 +2,7 @@
         const btn = document.querySelector('.btn');
         const searchBox = document.querySelector('.search-box');
         const weatherDisplay = document.querySelector('.weather-display');
-
+        let weatherImg = '';
         btn.addEventListener('click', () => {
             const input = document.querySelector('input');
             const cityName = input.value;
@@ -19,11 +19,34 @@
             timezone: data.timezone,
             temperature: data.main.temp,
            }
+           if(weatherApp.main === 'Snow') {
+             weatherImg = 'https://openweathermap.org/img/wn/10d@2x.png'; 
+        }
+        else if(weatherApp.main === 'Rain') {
+            weatherImg = './snow.jpg'; 
+        }else if(weatherApp.main === 'Clouds') {
+        weatherImg = 'https://openweathermap.org/img/wn/02d@2x.png'; 
+         }
+        else if(weatherApp.main === 'Mist' || weatherApp.main === 'Smoke' || weatherApp.main === 'Haze' || 
+        weatherApp.main === 'Dust' || weatherApp.main === 'Fog' || weatherApp.main === 'Sand' || weatherApp.main === 'Ash' ||
+        weatherApp.main === 'Squall' || weatherApp.main === 'Tornado') {
+    weatherImg = 'https://openweathermap.org/img/wn/50d@2x.png'; 
+}
+else if(weatherApp.main === 'Clear') {
+    weatherImg = 'https://openweathermap.org/img/wn/01d@2x.png'; 
+}
+else if(weatherApp.main === 'Drizzle') {
+    weatherImg = 'https://openweathermap.org/img/wn/09d@2x.png'; 
+}
+else if(weatherApp.main === 'Thunderstorm') {
+    weatherImg = 'https://openweathermap.org/img/wn/11d@2x.png'; 
+}
         weatherDisplay.style.display = 'block';
         weatherDisplay.innerHTML = `
         <h1>WEATHER FOR: <br>${(weatherApp.name).toUpperCase()}</h1>
         <p>COUNTRY: ${weatherApp.country}</p>
-        <p>Timezone: + ${weatherApp.timezone/3600} GMT</p>
+        <p>Timezone: ${weatherApp.timezone/3600} GMT</p>
+        <img width="200px" src="${weatherImg}" alt="Weather Image">
         <p>Weather: ${weatherApp.main}</p>
         <p>Temperature: ${(weatherApp.temperature-273).toFixed()} °C</p>
         <p>Pressure: ${weatherApp.pressure} h/Pa</p>`;
